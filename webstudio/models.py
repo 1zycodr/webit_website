6900f513d1cbc8743f9dd715c18f47713a6af953
1zycodr <5zamiggos@gmail.com>
2021-06-28 22:58:31 +0600

    initial commit

---
+++ webstudio/models.py
@@ -0,0 +1,41 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+class Works(models.Model):
+    work_photo_main=models.ImageField(null=True)
+    work_photo_2=models.ImageField(null=True)
+    work_photo_3=models.ImageField(null=True)
+    work_title=models.CharField(max_length=200,null=True)
+    work_info=models.TextField(null=True)
+    work_address=models.CharField(max_length=300,null=True,default='https://')
+    def __str__(self):
+        return self.work_title
+    
+
+class Order(models.Model):
+    order_email=models.CharField(max_length=200,null=True)
+    order_title=models.CharField(max_length=200,null=True)
+    order_number=models.CharField(max_length=200,null=True)
+    order_name=models.CharField(max_length=200,null=True)
+    order_info=models.TextField(null=True)
+    order_content=models.TextField(null=True)
+    order_date=models.DateTimeField(null=True,blank=True)
+
+    def send(self):
+        self.order_date=timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.order_title
+
+class Employee(models.Model):
+    employee_photo=models.ImageField(null=True)
+    employee_fullname=models.CharField(max_length=300,default='')
+    employee_specialization=models.CharField(max_length=300,default='')
+    employee_email=models.EmailField(max_length=300,default='example@mail.com')
+    employee_info=models.TextField(null=True)
+    
+    def __str__(self):
+        return self.employee_fullname
+# Create your models here.
---
+++ webstudio/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render
+from .forms import OrderForm
+from .models import Employee,Works
+
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+
+
+
+
+def main_page(request):
+    
+    administ = Employee.objects.filter(employee_specialization__contains = "Администрация")
+    front = Employee.objects.filter(employee_specialization = "Frontend-разработчики")
+    back = Employee.objects.filter(employee_specialization = "Backend-разработчики")
+    hr = Employee.objects.filter(employee_specialization = "HR-Менеджера")
+    designer = Employee.objects.filter(employee_specialization = "Дизайнеры")
+
+
+    works = Works.objects.all()
+
+
+
+
+
+    if request.method == 'POST':
+        # если метод POST, проверим форму и отправим письмо
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            order_title = form.cleaned_data['order_title']
+            order_email = form.cleaned_data['order_email']
+            info = form.cleaned_data['order_info']
+            info2 = form.cleaned_data['order_number']
+            info3 = form.cleaned_data['order_content']
+
+
+            info_end = "Имя: " + order_title+ " Что разработать?: " +  info + " Номер: " + info2 + " Описаине: " +  info3
+
+
+            
+            send_mail(
+                        order_email,
+                        info_end,
+                        'zinovievakinov1234@gmail.com',
+
+                        ['zinovievakinov1234@gmail.com'],
+
+                        fail_silently=True,
+                    )
+
+            return redirect('main_page')
+    else:
+
+        form = OrderForm()
+    return render(request,'webstudio/index.html',{'form':form,'administ':administ,'front':front,'back':back,'hr':hr,'works':works,'designer':designer})
+
+
